4ce13a92da6837923878e6708778b81bd76f6ce3
Rodolfo Ribeiro Gomes <rodolforg@gmail.com>
2019-03-07 23:26:29 -0300

    gdnative import plugin script checks library version
    Signed-off-by: Rodolfo Ribeiro Gomes <rodolforg@gmail.com>

--- demo/addons/psd_animation/psd_animation_import.gd
+++ demo/addons/psd_animation/psd_animation_import.gd
@@ -4,9 +4,15 @@ extends EditorPlugin
 var import_plugin
 
 func _enter_tree():
+	var PsdImporter = preload('res://addons/psd_animation/psd_importer.gdns').new()
+	if PsdImporter.get_version() != 2:
+		printerr('PsdImporter addon shared library version mismatch')
+		return
 	import_plugin = preload("import_plugin.gd").new()
 	add_import_plugin(import_plugin)
 
 func _exit_tree():
+	if not import_plugin:
+		return
 	remove_import_plugin(import_plugin)
 	import_plugin = null
